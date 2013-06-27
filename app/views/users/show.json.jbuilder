690121672ec76203c43515f9cd908f628d40198a
sadah <sadaaki.hirai@gmail.com>
2013-06-28 00:09:58 +0900

    my first rails4 app. scaffold user.

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :name, :email, :memo, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ScaffoldSample::Application.config.secret_key_base = 'f6b17e144522d029069f12bcc15b9622df8b42b70d84c353a9a52da9e136a5de22d5c9895becea2c36566d8bcd3dd0b782cad8f32afb3a69e4c38235c548a7e0'
